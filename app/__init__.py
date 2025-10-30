99d4507c1407cba2cabff4b6304e244069bc1fdf
mykhailomakoviichuk24-max <mykhailo.makoviichuk.24@pnu.edu.ua>
2025-10-30 13:09:55 +0200

    5

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from loguru import logger
+
+ 
+app = Flask(__name__)
+
+
+app.config.from_pyfile('../config.py')
+
+
+logger.add("logs/contact_form.log", rotation="10 MB", retention="1 month", level="INFO", 
+           format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}")
+
+
+from app.auth.views import auth_bp
+app.register_blueprint(auth_bp)
+
+
+from app import views
